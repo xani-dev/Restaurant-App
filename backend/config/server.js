7151237cd4993350aa15cad6186dbd6be88ae4f3
Xani <xani.design@gmail.com>
2021-09-16 13:17:00 -0400

    first commit

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'bf5f6f697dc4373469c26c03fddb90c0'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '49877a89-3e84-430b-a92d-74c6f4d614ce'
+};
